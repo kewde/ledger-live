8b24b4c586789005b7fe241b87457ef3a2fa3fac
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-07-20 17:46:48 +0200

    test: started dex test with ethereum account

--- apps/ledger-live-mobile/e2e/specs/swap/swap.spec.ts
+++ apps/ledger-live-mobile/e2e/specs/swap/swap.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "detox";
-import { loadConfig } from "../bridge/server";
-import PortfolioPage from "../models/wallet/portfolioPage";
-import SwapFormPage from "../models/trade/swapFormPage";
+import { loadConfig } from "../../bridge/server";
+import PortfolioPage from "../../models/wallet/portfolioPage";
+import SwapFormPage from "../../models/trade/swapFormPage";
 
 let portfolioPage: PortfolioPage;
 let swapPage: SwapFormPage;
